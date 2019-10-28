a350ff5827f8d5d3bedab6d04be1db680a396b99
Felix Nehrke <felix@nehrke.info>
2019-10-28 01:55:23 +0100

    Add team-product-recommendations

---
+++ team-product-recommendations/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {environment} from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  template: `
+      <div style="border: 3px dashed green; padding: 10px">
+        <router-outlet></router-outlet>
+      </div>
+  `,
+})
+export class AppComponent implements OnInit {
+  constructor(
+      private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.initialNavigation(); // Manually triggering initial navigation for @angular/elements ?
+
+    // Standalone mode
+    if (!environment.production) {
+      this.router.navigate(['/team-product-recommendations']);
+    }
+  }
+}
