50c526dce0b5ac223c84884e7a670f56c144137a
Felix Nehrke <felix@nehrke.info>
2019-10-28 02:36:03 +0100

    Hack browser history to use routing in components as well

--- team-shop-page/src/main.ts
+++ team-shop-page/src/main.ts
@@ -10,3 +10,19 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// Hack the browser history, to be able to listen to route-changes all the time
+(() => {
+  const pushState = window.history.pushState;
+  window.history.pushState = (state: any, title: string, url?: string) => {
+    pushState.call(history, state, title, url);
+    const push = new PopStateEvent('popstate', {state});
+    window.dispatchEvent(push);
+  };
+  const replaceState = window.history.replaceState;
+  window.history.replaceState = (state: any, title: string, url?: string) => {
+    replaceState.call(history, state, title, url);
+    const push = new PopStateEvent('popstate', {state});
+    window.dispatchEvent(push);
+  };
+})();
