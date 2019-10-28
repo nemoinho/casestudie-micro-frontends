5af7f3092f1a373fcdffaf8daecdf014faade699
Felix Nehrke <felix@nehrke.info>
2019-10-28 06:35:11 +0100

    Show nested components

--- team-product-recommendations/src/app/components/two-product-widget.component.ts
+++ team-product-recommendations/src/app/components/two-product-widget.component.ts
@@ -5,8 +5,10 @@ import {Component} from '@angular/core';
         <div style="border: 3px dashed green; padding: 10px; text-align: center">
             <p>Product Y</p>
             <img style="max-height: 250px" src="http://localhost:6100/y.png">
+            <team-shopping-cart-buy-button></team-shopping-cart-buy-button>
             <p>Product Z</p>
             <img style="max-height: 250px" src="http://localhost:6100/z.png">
+            <team-shopping-cart-buy-button></team-shopping-cart-buy-button>
         </div>
     `
 })
