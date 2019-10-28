ec4af2491a13ad271548cb89747a27c3731d8eca
Felix Nehrke <felix@nehrke.info>
2019-10-28 04:57:21 +0100

    Enable team-shopping-cart and team-product-recommendation

--- team-shop-page/src/app/app.component.ts
+++ team-shop-page/src/app/app.component.ts
@@ -13,16 +13,36 @@ export class AppComponent implements OnInit {
       url: 'http://localhost:6501',
       paths: ['runtime-es2015', 'main-es2015'],
       loaded: false
+    },
+    'team-product-recommendations': {
+      url: 'http://localhost:6502',
+      paths: ['runtime-es2015', 'main-es2015'],
+      loaded: false
+    },
+    'team-shopping-cart': {
+      url: 'http://localhost:6503',
+      paths: ['runtime-es2015', 'main-es2015'],
+      loaded: false
     }
   };
 
+  private queue = [];
+
   ngOnInit(): void {
-    this.load('team-product-details');
+    this.queue.push('team-product-details');
+    this.queue.push('team-product-recommendations');
+    this.queue.push('team-shopping-cart');
+    setTimeout(() => this.load(), 10);
   }
 
-  load(name: string) {
+  load() {
+    if (this.queue.length === 0) {
+      return;
+    }
+    const name = this.queue.shift();
     const configItem = this.config[name];
-    if (!configItem || configItem.loaded) {
+    if (!configItem) {
+      this.load();
       return;
     }
 
@@ -32,12 +52,13 @@ export class AppComponent implements OnInit {
 
     configItem.loaded = true;
     const content = document.getElementById('content');
-    configItem.paths.forEach(path => {
+    Promise.all(configItem.paths.map(path => new Promise(resolve => {
       const script = document.createElement('script');
       script.src = configItem.url + '/' + path + '.js';
       script.onerror = (e) => console.error('Failed to load', e);
+      script.onload = () => resolve();
       document.body.appendChild(script);
-    });
+    }))).then(() => setTimeout(() => this.load(), 0));
     const elem = document.createElement(name);
     content.appendChild(elem);
   }
